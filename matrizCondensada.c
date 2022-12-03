65ddfc7a179075588d4c3c2b6a7f828ea13ed786
Isadora <isadoravanco@gmail.com>
2022-12-02 23:21:59 -0300

    funcao MultiplicarMatrizes()

--- matrizCondensada.c
+++ matrizCondensada.c
@@ -17,6 +17,7 @@ typedef struct cel elemento;
 elemento* LerMatriz(FILE *arquivo);
 void EscreverMatriz(elemento *inicio);
 elemento* SomarMatrizes(elemento *inicioA, elemento *inicioB);
+elemento* MultiplicarMatrizes(elemento *inicioA, elemento *inicioB);
 int SomarElementosAbaixoDiagonal(elemento *inicio);
 void ApagarMatriz(elemento *inicio);
 
@@ -24,6 +25,7 @@ int main() {
     elemento* inicioA = NULL;
     elemento* inicioB = NULL;
     elemento* inicioC = NULL;
+    elemento* inicioD = NULL;
     FILE *arquivo;
     int elementosC;
     
@@ -44,14 +46,16 @@ int main() {
     EscreverMatriz(inicioB);
     
     printf("*****C*****\n");
-    //soma e escreve a matriz resultante
     inicioC = SomarMatrizes(inicioA, inicioB);
     elementosC = SomarElementosAbaixoDiagonal(inicioC);
-    printf("\n");
+    
+    printf("\n*****D*****\n");
+    inicioD = MultiplicarMatrizes(inicioA, inicioB);
     
     ApagarMatriz(inicioA);
     ApagarMatriz(inicioB);
     ApagarMatriz(inicioC);
+    ApagarMatriz(inicioD);
     
     return 0;
 }
@@ -99,7 +103,7 @@ elemento* LerMatriz(FILE *arquivo){
     return inicio;
 }
 
-//escreve as informações da matriz condensada e a matriz completa
+//escreve cada valor da matriz condensada em suas respectivas posições e a matriz completa
 void EscreverMatriz(elemento *inicio){
     elemento* aux = inicio;
     
@@ -132,7 +136,7 @@ void EscreverMatriz(elemento *inicio){
     printf("\n");
 }
 
-//soma as duas matrizes, escreve e retorna o ponteiro do inicio dela
+//soma as duas matrizes, escreve o resultado e retorna o ponteiro do inicio da resultante
 elemento* SomarMatrizes(elemento *inicioA, elemento *inicioB){
     elemento *inicioC = NULL;
     elemento *elementoAnterior, *novoElemento;
@@ -180,7 +184,76 @@ elemento* SomarMatrizes(elemento *inicioA, elemento *inicioB){
     return inicioC;
 }
 
-//soma os elementos abaixo da diagonal principal
+//multiplica as duas matrizes, escreve o resultado e retorna o ponteiro do inicio da resultante
+elemento* MultiplicarMatrizes(elemento *inicioA, elemento *inicioB){
+    elemento *auxA = inicioA; //guarda o endereço inicial de A
+    elemento *auxB = inicioB; //guarda o endereço inicial de B
+    
+    elemento *inicioC = NULL;
+    elemento *elementoAnterior, *novoElemento;
+
+    int somaElemento, multiplicacaoElemento;
+
+    for(int i = 0; i < N; i++){ //linha do elemento resultante
+        for(int j = 0; j < N; j++){ //coluna do elemento resultante
+            somaElemento = 0;
+            
+            for(int n = 0; n < N; n++){ //posição do elemento na linha/coluna
+                //retorna os ponteiros para o inicio de cada matriz
+                inicioA = auxA; 
+                inicioB = auxB;
+                multiplicacaoElemento = 0;
+                
+                while(inicioA != NULL){ //procura se tem o elemento na linha da matriz A
+                    if(inicioA -> i == i && inicioA -> j == n){
+                        multiplicacaoElemento = inicioA -> valor;
+                        break;
+                    }else{
+                        inicioA = inicioA -> proxElemento;
+                    }
+                }
+
+                while(inicioB != NULL){ //procura se tem o elemento na coluna da matriz B
+                    if(inicioB -> j == j && inicioB -> i == n){
+                        multiplicacaoElemento *= inicioB -> valor;
+                        break;
+                    }else{
+                        inicioB = inicioB -> proxElemento;
+                    }
+                }
+
+                if(inicioB == NULL){ //se não encontrou o elemento de B 
+                    multiplicacaoElemento = 0;
+                }
+                somaElemento += multiplicacaoElemento;
+            }
+
+            if(somaElemento != 0){ //armazena na lista ligada
+                novoElemento = malloc(sizeof(elemento));
+                novoElemento -> valor = somaElemento;
+                novoElemento -> i = i;
+                novoElemento -> j = j;
+                novoElemento -> proxElemento = NULL;
+                
+                // se o inicio ainda não aponta para algum endereço
+                // ou seja, é o primeiro elemento identificado,
+                // atribui o endereço do primeiro para o inicio
+                if(inicioC == NULL){
+                    inicioC = novoElemento;
+                    elementoAnterior = novoElemento;
+                }else{ //caso haja elementos existentes
+                    elementoAnterior -> proxElemento = novoElemento;
+                    elementoAnterior = novoElemento;
+                }
+            }
+        }
+    }
+
+    EscreverMatriz(inicioC);
+    return inicioC;
+}
+
+//soma os elementos abaixo da diagonal principal e retorna o resultado
 int SomarElementosAbaixoDiagonal(elemento *inicio){
     int resultado = 0;
 
